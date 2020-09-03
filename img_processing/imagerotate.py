edabbc3a62fb283c40381e61e0f5b7064a882e01
dhruvi29 <drdoshi29@gmail.com>
2020-09-03 12:30:20 +0530

    task1 completed

---
+++ img_processing/imagerotate.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+from PIL import Image as im
+import math
+
+img=im.open("rotate.png")
+hexme=np.array(img)
+h,w,c=hexme.shape
+don=np.zeros((h+500,w+500,c),dtype=np.uint8)
+radians=math.pi/3
+c, s = math.cos(radians), math.sin(radians)
+lol = np.array([[c, -s], [s, c]])
+for i in range(h):
+    if(radians==math.pi):
+        don=hexme[::-1]
+        break
+    for j in range(w):
+        n=np.array([i-h/2,j-w/2]).reshape(2,1)
+        m=np.dot(lol,n)
+        x,y=int(m[0]+(h/2)),int(m[1]+(h/2))
+        # don[i,j]=hexme[i,j]
+        if(x>=0 and y>=0 and x<822 and y<1100):
+            don[x,y]=hexme[i,j]
+po = im.fromarray(don)
+im._show(po)
+# print(don)
